2e5eeb0a04ceab510f2682c360bb6639aced233e
dinara0708 <aikenova01@gmail.com>
2018-09-01 19:48:37 +0600

    -

--- HW_functionPointers_29.08.2018/Source.cpp
+++ HW_functionPointers_29.08.2018/Source.cpp
@@ -14,8 +14,79 @@ using namespace std;
 // Простое число – это число, которое делиться ТОЛЬКО на 1 и на себя(2, 5, 7, 11 и т.д.).
 // Рекомендации:
 // Сначала посчитайте количество простых элементов в массиве, потом уже создайте новый массив и занесите туда эти числа.
+bool prime(int a) {
+	if (a < 0 || a == 0 || a == 1) return false;
+	for (int i = 2; i < a; i++)
+	{
+		if (a%i == 0) return false;
+	}
+	return true;
+}
+void arr_prime(int* &a, int &n)
+{
+	int k = 0;
+	for (int i = 0; i < n; i++)
+		if (prime(a[i])) k++;
 
+	if (k == 0)
+	{
+		cout << "No prime numbers" << endl << endl;
+		return;
+	}
 
+	int *tmp = new int[k];
+	int j = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (prime(a[i]))
+			tmp[j++] = a[i];
+	}
+	delete[] a;
+	a = tmp;
+	n = k;
+}
+void arr_3(int* &a, int &n)
+{
+	int k = 0;
+	for (int i = 0; i < n; i++)
+		if (a[i] % 3 == 0) k++;
+
+	int *tmp = new int[k];
+	int j = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i] % 3 == 0)
+			tmp[j++] = a[i];
+	}
+	delete[] a;
+	a = tmp;
+	n = k;
+}
+void arr_even(int* &a, int &n)
+{
+	int k = 0;
+	for (int i = 0; i < n; i++)
+		if (a[i] % 2 == 0) k++;
+
+	int *tmp = new int[k];
+	int j = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i] % 2 == 0)
+			tmp[j++] = a[i];
+	}
+	delete[] a;
+	a = tmp;
+	n = k;
+}
+void f3(int *&a, int &n, void(*f)(int*&, int&)) {
+	f(a, n);
+	for (int i = 0; i < n; i++)
+	{
+		cout << a[i] << " ";
+	}
+	cout << endl;
+}
 int main()
 {
 	srand(time(NULL));
@@ -33,12 +104,26 @@ int main()
 		{
 		case 1:
 		{
+			int *a;
+			int n = 10;
+			a = new int[n];
 
-		}
-		break;
-		case 2:
-		{
+			for (int i = 0; i < n; i++)
+			{
+				a[i] = rand() % 100 - 20;
+				cout << a[i] << " ";
+			}
+			cout << endl;
 
+			void(*f[3])(int *&, int&) = { arr_prime, arr_3, arr_even };
+
+			int choice;
+			cin >> choice; // нельзя зациклить, т.к. каждую итерацию использует указатель на новый массив
+			// если же в f3 не передавать по ссылкеб возникнет конфликт
+
+			f3(a, n, *f[choice]);
+	
+			delete[] a;
 		}
 		break;
 		default:
